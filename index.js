a1fcf5b69b909a4bfdda23a283247423a2018cd9
palacharlanarendra <palacharlanarendra@gmail.com>
2023-10-30 08:41:20 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//there is no attach that is happpening
+//there is no prototype, no window , no global
+//
+
+
+function countWords(givenString){
+    // let para = this.toString();
+    let splittedPara = givenString.split(" ");
+    return splittedPara.length
+}
+
+
+export {countWords};
